5da1dc53c22713a7b96677ed5a8b854942f87f33
Robin <robin@mobiux.in>
2024-10-24 16:54:06 +0530

    rebase entire code

--- components/common/Highlight.jsx
+++
@@ -1,9 +0,0 @@
-const Highlight = ({ children }) => {
-    return (
-        <span className='px-2 py-1 mr-1 bg-black/20 text-black/50 rounded-sm'>
-            {children}
-        </span>
-    )
-}
-
-export default Highlight
--- pages/404.js
+++
@@ -1,9 +0,0 @@
-import PageNotFound from '@/components/PageNotFound';
-
-const Page404 = () => {
-    return (
-        <PageNotFound />
-    );
-};
-
-export default Page404;
