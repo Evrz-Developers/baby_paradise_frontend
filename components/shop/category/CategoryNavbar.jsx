e983883b93fef0841a2fea3af813c7e6605f3b0f
Robin K <dev.robinkurian@email.com>
2024-11-15 01:24:21 +0530

    style: category navbar

--- components/shop/category/CategoryNavbar.jsx
+++ components/shop/category/CategoryNavbar.jsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 const CategoryNavbar = ({ categories }) => {
     return (
-        <nav className="flex bg-white px-4 py-2 md:text-base xxs:text-xs border-2 border-gray-300 overflow-hidden">
+        <nav className="flex bg-white px-4 pt-2 py-1 md:text-base xxs:text-xs border-b-2 border-gray-200 overflow-hidden">
             <div className="flex justify-between items-center overflow-x-auto ">
                 <div className="flex space-x-4 ">
                     {categories.map((category) => (
-                        <Link key={category.id} href={`/category/${category.id}`} className="text-gray-700 hover:text-gray-950 transition duration-300 whitespace-nowrap">
+                        <Link key={category.id} href={`/category/${category.id}`} className="bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-950 transition duration-300 whitespace-nowrap rounded-full px-4 py-2">
                             {category.name}
                         </Link>
                     ))}
