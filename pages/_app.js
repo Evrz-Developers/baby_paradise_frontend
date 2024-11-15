a3b781ec32a9d7711e20698ff4dc4c7ec3be73e0
Robin K <dev.robinkurian@email.com>
2024-11-15 23:56:06 +0530

    change the image url for og

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta property="og:title" content="Baby Paradise" />
         <meta property="og:description" content="Baby Paradise is a one stop shop for all your kids' needs." />
-        <meta property="og:image" content="/images/logo.jpg" />
+        <meta property="og:image" content="https://babyparadisestore.netlify.app/images/logo.jpg" />
         <meta property="og:url" content="https://babyparadisestore.netlify.app" />
         <meta property="og:type" content="website" />
       </Head>
