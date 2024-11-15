656ec64fb5b9473e6d8776aa4207179ab82b4609
Robin K <dev.robinkurian@email.com>
2024-11-15 23:18:13 +0530

    opengraph meta data and favicon

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import Dashboard from "@/components/shop/Dashboard/Dashboard";
+import Head from 'next/head';
 
 const Home = () => {
   return (
@@ -10,6 +11,13 @@ const Home = () => {
         "min-h-[calc(100vh-1px)]  flex flex-col items-center justify-center h-full w-full"
       }
     >
+      <Head>
+        <meta property="og:title" content="Baby Paradise" />
+        <meta property="og:description" content="Baby Paradise is a one stop shop for all your kids' needs." />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:url" content="https://babyparadisestore.netlify.app" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Dashboard />
     </DefaultLayout>
   );
