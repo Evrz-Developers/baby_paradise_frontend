3673dace012cfaf9b8e3e3fda630ec58cd69a5d4
Robin K <dev.robinkurian@email.com>
2024-11-15 23:33:00 +0530

    add og to _app for global metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '@/styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Baby Paradise" />
+        <meta property="og:description" content="Baby Paradise is a one stop shop for all your kids' needs." />
+        <meta property="og:image" content="/images/logo.png" />
+        <meta property="og:url" content="https://babyparadisestore.netlify.app" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,6 @@ const Home = () => {
         "min-h-[calc(100vh-1px)]  flex flex-col items-center justify-center h-full w-full"
       }
     >
-      <Head>
-        <meta property="og:title" content="Baby Paradise" />
-        <meta property="og:description" content="Baby Paradise is a one stop shop for all your kids' needs." />
-        <meta property="og:image" content="favicon.ico" />
-        <meta property="og:url" content="https://babyparadisestore.netlify.app" />
-        <meta property="og:type" content="website" />
-      </Head>
       <Dashboard />
     </DefaultLayout>
   );
