84deb0a81d8d1b8c860c5ee2486aed703ff0bca8
Robin K <dev.robinkurian@email.com>
2024-11-15 23:28:05 +0530

    fix favicon url

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = () => {
       <Head>
         <meta property="og:title" content="Baby Paradise" />
         <meta property="og:description" content="Baby Paradise is a one stop shop for all your kids' needs." />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:image" content="favicon.ico" />
         <meta property="og:url" content="https://babyparadisestore.netlify.app" />
         <meta property="og:type" content="website" />
       </Head>
