a8d24c5e84fcd67dd290c3fc9f791136100e5e5a
Robin K <dev.robinkurian@email.com>
2024-11-15 22:31:40 +0530

    fix image size in image grid

--- components/shop/product/ProductGrid.jsx
+++ components/shop/product/ProductGrid.jsx
@@ -9,7 +9,7 @@ const ProductGrid = ({ items, handleListItemClick, title, className }) => {
             <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"}>
                 {items.map((product) => (
                     <div key={product.id} className="flex flex-col p-4 bg-white items-center rounded-[4px] cursor-pointer" onClick={() => handleListItemClick(product.id)}>
-                        <div className='relative w-40 md:w-full h-40'>
+                        <div className='relative w-48 md:w-full h-60'>
                             <Image
                                 src={product.product_image ?? "/images/shop/noImageplacholder.jpg"}
                                 alt="product image"
@@ -24,7 +24,7 @@ const ProductGrid = ({ items, handleListItemClick, title, className }) => {
                             />
                         </div>
                         <div className="flex flex-col items-center mt-2 mb-1 ">
-                            <p className="text-lg font-semibold">{product.name}</p>
+                            <p className="text-lg ">{product.name}</p>
                             <p className="text-lg font-semibold">{product.retail_price}</p>
                         </div>
                     </div>
