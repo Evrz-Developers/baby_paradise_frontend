f3ecea8a57d8c4fee1791eccdda1e14021a8f611
Robin K <dev.robinkurian@email.com>
2024-11-16 19:42:31 +0530

    use the loader on top of images in product image grid

--- components/shop/product/ProductGrid.jsx
+++ components/shop/product/ProductGrid.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import { CustomimageLoader } from '@/components/common/CustomImageLoader';
+import Loader from '@/components/common/Loader';
 
 const ProductGrid = ({ items, handleListItemClick, title, className }) => {
+    const [loadingId, setLoadingId] = useState(null);
+
+    const handleClick = (id) => {
+        setLoadingId(id);
+        handleListItemClick(id);
+    };
+
     return (
         <div className={`xxs:max-h-[calc(100vh-150px)] md:max-h-[calc(100vh-100px)] ${className}`}>
             <h2>{title}</h2>
             <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"}>
                 {items.map((product) => (
-                    <div key={product.id} className="flex flex-col bg-white items-start rounded-[4px] cursor-pointer" onClick={() => handleListItemClick(product.id)}>
+                    <div key={product.id} className="flex flex-col bg-white items-start rounded-[4px] cursor-pointer" onClick={() => handleClick(product.id)}>
                         <div className='relative w-48 md:w-full h-60'>
+                            {loadingId === product.id && <Loader className="bg-opacity-30" />}
                             <Image
                                 src={product.product_image ?? "/images/shop/noImageplacholder.jpg"}
                                 alt="product image"
@@ -17,6 +26,7 @@ const ProductGrid = ({ items, handleListItemClick, title, className }) => {
                                 fill
                                 style={{
                                     objectFit: 'cover',
+                                    zIndex: 1,
                                 }}
                                 loader={CustomimageLoader}
                                 unoptimized
